a13eaadcb72f4980c95f3456a97e2f76605f8517
npierre <nicolas.pierre@cern.ch>
2018-11-14 14:19:48 +0100

    Fix.

--- GetFlux.h
+++ GetFlux.h
@@ -8,6 +8,9 @@
 #include <set>
 #include <string>
 
-
+static int bsl_select(const struct dirent *entry);
+void fill_badspills();
+int get_flux(int run_min, int run_max, int charge, float half_win,
+    Double_t& flux_tot, Double_t& eflux_tot);
 
 #endif
